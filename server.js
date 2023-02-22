c9871459b2b84880620e0d9e7adfaf650c8a4a2e
thomasKim <thomaskim08310@gmail.com>
2023-02-21 19:20:23 -0500

    setup routers

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const dotenv = require('dotenv')
 
+//Route files
+const bootcamps = require('./routes/bootcamps')
 //load env vars
 dotenv.config({
     path: './config/config.env'
@@ -8,26 +10,8 @@ dotenv.config({
 
 const app = express()
 
-app.get('/api/v1/bootcamps', (req, res) =>{
-    res.status(200).json({ success: true, msg: 'show all bootcamps'})
-})
-
-app.get('/api/v1/bootcamps/:id', (req, res) =>{
-    res.status(200).json({ success: true, msg: `get single bootcamp ${req.params.id}`})
-})
-
-app.post('/api/v1/bootcamps', (req, res) =>{
-    res.status(200).json({ success: true, msg: 'Create new bootcamp'})
-})
-
-app.put('/api/v1/bootcamps/:id', (req, res)=>{
-    res.status(200).json({ success: true, msg: `Update bootcamp ${req.params.id}`})
-})
-
-app.delete('/api/v1/bootcamps/:id', (req, res)=>{
-    res.status(200).json({ success: true, msg: `Delete bootcamp ${req.params.id}`})
-})
-
+//mount routers
+app.use('/api/v1/bootcamps', bootcamps)
 
 const PORT = process.env.PORT || 5000
 
