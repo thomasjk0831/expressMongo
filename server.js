24c4ffd6c6e82f885702dced75060d0bf70dd85c
thomasKim <thomaskim08310@gmail.com>
2023-02-22 19:18:57 -0500

    setup logger middleware

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const logger = require('./middleware/logger')
 
 //Route files
 const bootcamps = require('./routes/bootcamps')
@@ -10,6 +11,7 @@ dotenv.config({
 
 const app = express()
 
+app.use(logger)
 //mount routers
 app.use('/api/v1/bootcamps', bootcamps)
 
