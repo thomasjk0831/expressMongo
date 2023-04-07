0e79878abd071b0f2c7ff2e06f91acc0f0c6d9f0
thomasKim <thomaskim08310@gmail.com>
2023-04-07 18:09:49 -0400

    fetching bootcamps

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -3,14 +3,31 @@ const Bootcamp = require('../models/Bootcamp')
 //@desc     Get all bootcamps
 //@route    GET /api/v1/bootcamps
 //@access   Public
-exports.getBootcamps = (req, res, next) => {
-    res.status(200).json({ success: true, msg: 'show all bootcamps'})
+exports.getBootcamps = async (req, res, next) => {
+    try{
+       const bootcamps = await Bootcamp.find()
+       res.status(200).json({
+          success: true,
+          data: bootcamps
+       })
+    }catch(err){
+       res.status(400).json({ success:false})
+    }
 }
 
 //@desc     Get single bootcamp
 //@route    GET /api/v1/bootcamps/:id
 //@access   Public
-exports.getBootcamp = (req, res, next) => {
+exports.getBootcamp = async (req, res, next) => {
+    try{
+       const bootcamp = await Bootcamp.findById(req.params.id)
+       if(!bootcamp){
+        res.status(400).json({ success:false })
+       }
+       res.status(200).json({ success: true, data: bootcamp })
+    }catch(err){
+       res.status(400).json({ success: false })
+    }
     res.status(200).json({ success: true, msg: `get single bootcamp ${req.params.id}`})
 }
 
